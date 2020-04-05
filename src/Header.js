c257c2a7e4de380b40dfd0f2bd6df0c3931a63b5
rajat7707 <42389833+rajat7707@users.noreply.github.com>
2020-04-05 12:08:31 +0530

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
 class Header extends Component{
+
   render(){
     return(
       <nav className="navbar navbar-expand-lg navbar-light bg-light card-header">
           <div className="navbar-nav">
             <NavLink className="nav-item nav-link" exact to = "/todos" activeStyle = {{color : "blue"}} >Todos</NavLink>
-            <NavLink className="nav-item nav-link" exact to = "/"  activeStyle = {{color : "blue"}}  >Users</NavLink>
+            <NavLink className="nav-item nav-link" exact to = "/user"  activeStyle = {{color : "blue"}}  >Users</NavLink>
           </div>           
       </nav>
     )
